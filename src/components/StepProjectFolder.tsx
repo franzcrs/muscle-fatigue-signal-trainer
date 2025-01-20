3cac10844afbfdea9c074b24d31a827836f71b32
Franz Chuquirachi <franz.chuquirachi@gmail.com>
2025-01-21 00:40:06 +0900

    Folder selection with native dialog implemented with tauri plugin

--- src/components/StepProjectFolder.tsx
+++ src/components/StepProjectFolder.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Search, ChevronsUpDown, FolderOpen } from 'lucide-react'
+import { open } from '@tauri-apps/plugin-dialog';
+import { BaseDirectory, resourceDir, dirname } from '@tauri-apps/api/path';
+import { invoke } from '@tauri-apps/api/core';
 
 type StepProjectFolderProps = {
   folderPath: string;
@@ -8,24 +11,71 @@ type StepProjectFolderProps = {
   setIsFolderOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+/**
+ * Resolves all directories defined in the API's Path BaseDirectory enum.
+ */
+async function resolveAllDirectories() {
+  const promises = Object.values(BaseDirectory)
+    .filter((value) => typeof value === 'number') // Filter numeric values (enum entries)
+    .map((directory) =>
+      invoke('plugin:path|resolve_directory', { directory })
+        .then((path) => ({ directory, path }))
+        .catch((error) => ({ directory, error: error.message }))
+    );
+
+  const results = await Promise.all(promises);
+
+  results.forEach((result) => {
+    if (result.path) {
+      console.log(`Directory ${result.directory}: ${result.path}`);
+    } else {
+      console.error(`Error resolving directory ${result.directory}: ${result.error}`);
+    }
+  });
+}
+
 const StepProjectFolder = ({ folderPath, setFolderPath, isFolderOpen, setIsFolderOpen }: StepProjectFolderProps) => {
   const [folderName, setFolderName] = useState('Choose your folder');
 
-  const selectFolder = () => {
-    const folderPath = '.../muscle_fatigue_isometric_elbow_flexion';
+  const selectFolder = async (folderPath: string | null) => {
+    // const folderPath = '.../muscle_fatigue_isometric_elbow_flexion';
+    if (!folderPath) return;
     let folderName = folderPath.split('/').pop();
+    const folderParent = await dirname(folderPath)
+      .then((path) => path.split('/').pop())
+      .catch((error) => console.error(error));
+    const folderPathDisplay = '.../' + folderParent as string + '/' + folderName;
     folderName
       ? folderName = folderName
         .replace(/_/g, ' ')
         .replace(/\b\w/g, char => char.toUpperCase())
-      : folderName = folderPath;
-    setFolderPath(folderPath);
+      : folderName = folderPathDisplay;
+    setFolderPath(folderPathDisplay);
+    console.log('Folder path set to:', folderPathDisplay);
     setFolderName(folderName);
+    console.log('Folder name set to:', folderName);
+    setIsFolderOpen(true);
   }
 
-  const handlerClick = () => {
-    selectFolder();
-    setIsFolderOpen(true);
+  const handlerClick = async () => {
+    // Evaluate the retrievable directories from path API
+    console.log('Verify the BaseDirectory Members at: https://github.com/tauri-apps/tauri/blob/dev/packages/api/src/path.ts');
+    resolveAllDirectories();
+    // Resolve the resource directory which is also the root folder of the binary
+    const resourceDirPath = await resourceDir();
+    console.log('api.path.resourceDir()', resourceDirPath);
+    // Open a Directory selection dialog
+    const directoryPath = await open({
+      canCreateDirectories: true,
+      // defaultPath: '/Users/franz/Documents/master_thesis/muscle-fatigue-signal-trainer',
+      defaultPath: resourceDirPath,
+      directory: true,
+      multiple: false,
+      title: 'Confirm your project folder',
+    });
+    console.log('plugin-dialog.open()', directoryPath);
+    // Set the project folder according to the user selection
+    selectFolder(directoryPath);
   }
 
   return (
